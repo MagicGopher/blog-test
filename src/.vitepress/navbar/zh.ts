6205fbc3aa9e4e8781eed08f7de4b4d9b7d895b9
matrix <gopher997@gmail.com>
2024-06-25 03:53:57 +0800

    feat: 博客加入记录更新日志功能

--- src/.vitepress/navbar/zh.ts
+++ src/.vitepress/navbar/zh.ts
@@ -3,4 +3,12 @@ import { DefaultTheme } from "vitepress";
 export const zhNavbar: DefaultTheme.Config['nav'] = [
     // 首页
     { text: '首页', link: '/' },
+    // 关于我
+    {
+        text: '关于我',
+        items: [
+            { text: '我的信息', link: '/docs/zh/关于我/我的信息.md' },
+            { text: '更新日志', link: '/docs/zh/关于我/更新日志.md' },
+        ]
+    }
 ]
